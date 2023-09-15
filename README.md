9919f1ca5cefea3da1dfa0239c6b364b055a0bc4
MaxKingPor <1512347620@qq.com>
2023-09-15 11:14:27 +0800

    msvc 环境报错解决

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # GMSSL rust 绑定
 
-* [GmSSL-3.1.0](https://github.com/guanzhi/GmSSL)
+* 此仓库为[GmSSL-3.1.0](https://github.com/guanzhi/GmSSL)的绑定 采用静态链接
+
+# Features
+* `ENABLE_SM2_ALGOR_ID_ENCODE_NULL` Enable AlgorithmIdenifier with algorithm sm2sign_with_sm3 encode a NULL object as parameters
+* `ENABLE_SM2_PRIVATE_KEY_EXPORT` Enable export un-encrypted SM2 private key
+* `ENABLE_TLS_DEBUG` Enable TLS and TLCP print debug message
+* `ENABLE_SM3_AVX_BMI2` Enable SM3 AVX+BMI2 assembly implementation
+* `ENABLE_SM4_AESNI_AVX` Enable SM4 AESNI+AVX assembly implementation
+* `ENABLE_SM2_EXTS` Enable SM2 Extensions
+* `ENABLE_BROKEN_CRYPTO`  Enable broken crypto algorithms
+* `ENABLE_RDRND` Enable Intel RDRND instructions
+* `ENABLE_GMT_0105_RNG` Enable GM/T 0105 Software RNG
+* `ENABLE_HTTP_TESTS` Enable HTTP GET/POST related tests
--- build.rs
+++ build.rs
@@ -1,18 +1,59 @@
-use std::{env, path::PathBuf};
+use std::env;
+use std::path::PathBuf;
 
 fn main() {
     // 编译GmSSL
-    let path = cmake::Config::new("GmSSL")
+    let mut config = cmake::Config::new("GmSSL");
+    config
         .define("ENABLE_SM2_EXTS", "ON") // sm2 扩展
         .define("BUILD_SHARED_LIBS", "OFF") // 静态链接
         .configure_arg("--no-warn-unused-cli") // 交叉编译时防止cmake 报错
-        .build_target("gmssl") // 仅仅编译库
-        .build();
-    println!("cargo:rerun-if-changed=GmSSL");
+        .build_target("gmssl"); // 仅仅编译库
+
+    env::vars()
+        .filter_map(|(k, _)| {
+            if k.starts_with("CARGO_FEATURE_ENABLE") && k != "CARGO_FEATURE_DEFAULT" {
+                Some(k.replace("CARGO_FEATURE_", ""))
+            } else {
+                None
+            }
+        })
+        .for_each(|arg| {
+            config.define(arg, "ON");
+        });
+
+    if cfg!(target_env = "msvc") {
+        config.cflag("/utf-8"); // msvc 有编码问题统一使用UTF-8
+        if cfg!(target_family = "windows") {
+            config.cflag("/DWIN32 /D_WINDOWS");
+        }
+    };
+    let path = config.build();
     println!(
         "cargo:rustc-link-search=native={}/build/bin",
         path.display()
     );
+    if cfg!(target_env = "msvc") {
+        println!(
+            "cargo:rustc-link-search=native={}/build/bin/Debug",
+            path.display()
+        );
+        println!(
+            "cargo:rustc-link-search=native={}/build/bin/Release",
+            path.display()
+        );
+        println!(
+            "cargo:rustc-link-search=native={}/build/bin/MinSizeRel",
+            path.display()
+        );
+        println!(
+            "cargo:rustc-link-search=native={}/build/bin/RelWithDebInfo",
+            path.display()
+        );
+    }
+
+    println!("cargo:rerun-if-changed=GmSSL");
+
     println!("cargo:rustc-link-lib=static=gmssl"); // 链接
 
     // 生成绑定
@@ -27,13 +68,10 @@ fn main() {
         // .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
-    // env::var("OUT_DIR").unwrap()
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
 
     println!("cargo:rerun-if-changed=wrapper.h");
-
-    // let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 }
